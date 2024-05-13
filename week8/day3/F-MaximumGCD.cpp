65692fe56aca5d6b9b5e47081e7897392869d759
MDMinhazulIslamKhan <minhazulislamkhan@gmail.com>
2024-05-14 00:23:20 +0600

    week-8 : day-3

---
+++ week8/day3/F-MaximumGCD.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        cout << (n / 2) << endl;
+        // for (int i = 1; i < n; i++)
+        // {
+        //     for (int j = i + 1; j <= n; j++)
+        //     {
+        //         cout << i << " " << j << endl;
+        //     }
+
+        // }
+
+    }
+
+    return 0;
+}
